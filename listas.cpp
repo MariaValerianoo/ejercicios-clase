4fba60fa4d7993721a10579ef9c57382b14df60f
MariaValerianoo <149009830+MariaValerianoo@users.noreply.github.com>
2024-03-13 14:12:54 -0500

    update

---
+++ listas.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+template<typename T>
+class Nodo{
+    private:
+    T dato;
+    Nodo*next;
+
+
+    public:
+    Nodo(){
+        next=NULL;
+    }
+    Nodo (T d){
+        dato=d;
+        next=NULL;
+    }
+    T get_dato(){
+        return dato;
+    }
+    void set_dato(T d){
+        dato=d;
+    }
+    Nodo<T>* get_next(){
+        return next;
+    }
+    void set_next(Nodo<T>*next){
+        this->next=next;
+
+    }
+
+
+} 
