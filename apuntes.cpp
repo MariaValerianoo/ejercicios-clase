1b596baba7e9a0bf9d01fa71f91515e08fa962fc
MariaValerianoo <149009830+MariaValerianoo@users.noreply.github.com>
2024-03-13 14:49:05 -0500

    Update apuntes.cpp

--- apuntes.cpp
+++ apuntes.cpp
@@ -2,3 +2,15 @@
 //int myFunction(int x)
 // float myFunction (float x)
 // double myFunction(double x, double y)
+//listas->multilistas
+/*reglas de las listas:
+las listas tienen que ser finitas
+lineales
+ordenada
+las listas no se crean ordenadas sino por posicion
+operaciones que puedo hacer en las listas: 
+crear,agregar,insertar,buscar,modificar,++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+crear listas
+push_back(elemento)
+TAREA: LISTA DE CANCIONES, CREAR UNA LISTA DE REPRODUCCION, LISTA ENLAZADA CON MI AMIGO
+TIPO SPOTIFY (JAM)*/
